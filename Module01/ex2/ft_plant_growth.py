5297798deff36a2a8db40602a7eb959b5720dc6e
Aghvan Aleksanyan <agaleksa@c2r7s6.42yerevan.am>
2026-03-22 21:08:01 +0400

    start Module01

---
+++ Module01/ex2/ft_plant_growth.py
@@ -0,0 +1,27 @@
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name: str = name
+        self.height: float = height
+        self.age: int = age
+
+    def grow(self, cm: float = 0.8) -> None:
+        self.height += cm
+
+    def age_plant(self, days: int = 1) -> None:
+        self.age += days
+    
+    def show(self) -> None:
+        print(f"{self.name}: {round(self.height,1)}cm, {self.age} days old")
+
+if __name__ == "__main__":
+    rose = Plant("Rose", 25.0, 30)
+    print("=== Garden Plant Growth ===")
+
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        rose.grow()
+        rose.age_plant()
+        rose.show()
+
+    growth = round(rose.height - 25.0)
+    print(f"Growth this week: {growth}cm")
