41d0b17a763dcd0426ae05583a1d03ee82af88d8
Gayane Aleksanyan <gayanealeksanyan@Gayanes-MacBook-Pro.local>
2026-03-23 20:17:43 +0400

    add ex3, ex4

---
+++ Module01/ex4/ft_garden_security.py
@@ -0,0 +1,49 @@
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+        self._height = 0.0
+        self._age = 0
+
+        self.set_height(height)
+        self.set_age(age)
+
+    def show(self) -> None:
+        print(f"{self.name}: {self._height:.1f}cm, {self._age} days old")
+
+    def set_height(self, height: float) -> None:
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            print("Height update rejected")
+        else:
+            self._height = height
+            print(f"Height updated: {height}cm")
+
+    def get_height(self) -> float:
+        return self._height
+
+    def set_age(self, age: int) -> None:
+        if age < 0:
+            print(f"{self.name}: Error, age can't be negative")
+            print("Age update rejected")
+        else:
+            self._age = age
+            print(f"Age updated: {age} days")
+
+    def get_age(self) -> int:
+        return self._age
+
+
+if __name__ == "__main__":
+    print("=== Garden Security System ===")
+
+    plant = Plant("Rose", 15.0, 10)
+    plant.show()
+
+    plant.set_height(25)
+    plant.set_age(30)
+
+    plant.set_height(-5)
+    plant.set_age(-10)
+
+    print("Current state:", end=" ")
+    plant.show()
