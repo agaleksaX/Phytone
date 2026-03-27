8f5b54d6686528445a82a396bd35d6c4ef27b26d
Gayane Aleksanyan <gayanealeksanyan@Gayanes-MacBook-Pro.local>
2026-03-27 08:17:13 +0400

    ex0

--- Module01/ex0/ft_garden_intro.py
+++ Module01/ex0/ft_garden_intro.py
@@ -4,9 +4,9 @@ def ft_garden_intro():
     age: int = int(input("Enter age plant: "))
 
     print("=== Welcome to My Garden ===")
-    print(f"Plant: {name}")
-    print(f"Height: {height}cm")
-    print(f"Age: {age} days")
+    print("Plant: ", name)
+    print("Height: ", height, "cm")
+    print("Age: ", age, " days")
     print("=== End of Program ===")
 
 if __name__ == "__main__":
