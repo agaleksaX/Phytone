8bcddfb93de011ce44921924e9a51be8a8c55263
Gayane Aleksanyan <gayanealeksanyan@Gayanes-MacBook-Pro.local>
2026-05-13 23:05:32 +0400

    co,pleat ex1

---
+++ Module07/ex1/ex1/__init__.py
@@ -0,0 +1,7 @@
+from .transform_family import TransformCreatureFactory
+from .healing_family import HealingCreatureFactory
+
+__all__ = [
+    "HealingCreatureFactory",
+    "TransformCreatureFactory",
+]
---
+++ Module07/ex1/ex1/transform_family.py
@@ -0,0 +1,53 @@
+from .capabilities import TransformCapability
+from ex0.ex0.creature import CreatureFactory, Creature
+
+
+class Shiftling(Creature, TransformCapability):
+
+    def __init__(self) -> None:
+        Creature.__init__(self, "Shiftling", "Normal")
+        TransformCapability.__init__(self)
+
+    def attack(self) -> str:
+        if self.transformed:
+            return "Shiftling performs a boosted strike!"
+        else:
+            return "Shiftling attacks normally."
+
+    def transform(self) -> str:
+        self.transformed = True
+        return "Shiftling shifts into a sharper form!"
+
+    def revert(self) -> str:
+        self.transformed = False
+        return "Shiftling returns to normal."
+
+
+class Morphagon(Creature, TransformCapability):
+
+    def __init__(self) -> None:
+        Creature.__init__(self, "Morphagon", "Normal/Dragon")
+        TransformCapability.__init__(self)
+
+    def attack(self) -> str:
+        if self.transformed:
+            return "Morphagon unleashes a devastating morph strike!"
+        else:
+            return "Morphagon attacks normally."
+
+    def transform(self) -> str:
+        self.transformed = True
+        return "Morphagon morphs into a dragonic battle form!"
+
+    def revert(self) -> str:
+        self.transformed = False
+        return "Morphagon stabilizes its form."
+
+
+class TransformCreatureFactory(CreatureFactory):
+
+    def create_base(self) -> Creature:
+        return Shiftling()
+
+    def create_evolved(self) -> Creature:
+        return Morphagon()
